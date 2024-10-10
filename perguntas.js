0b2ac48eb9117c175f10343daee162e6a02921c0
lacerdanicole <160140059+lacerdanicole@users.noreply.github.com>
2024-10-10 11:25:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Princesas',
+    'Qual princesa perdeu o sapato de cristal?',
+    'Cinderela'
+)
+
+criaCartao(
+    'Princesas',
+    'Qual princesa mora com sete anões?',
+    'Branca de Neve'
+)
+
+criaCartao(
+    'Princesas',
+    'Qual princesa perdeu sua voz como consequência para se casar com um principe?',
+    'Ariel'
+)
+
+criaCartao(
+    'Princesas',
+    'Qual princesa teve a mãe transformada em urso?',
+    'Mérida.'
+)
+
+
+criaCartao(
+    'Princesas',
+    'Qual princesa beijou um sapo?',
+    'Tiana.'
+)
+
+criaCartao(
+    'Princesas',
+    'Qual princesa ?',
+    'Tiana.'
+)
